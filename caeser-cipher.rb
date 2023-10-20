d9a9dd9cfb62bfcd548cb2f821e4039a4786afc5
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-10-20 15:18:29 -0700

    Fix grammar and spacing issues

--- caeser-cipher.rb
+++ caeser-cipher.rb
@@ -1,24 +1,18 @@
 def ceaser_cipher(text,num)
-    cipher_text = ''
-    text.each_char do |c|
-        if /[A-Z]/.match?(c)
-            new_ascii = c.ord + num
-            if new_ascii > 90
-                new_ascii = new_ascii - 26
-            end
-            cipher_text += new_ascii.chr
-        elsif /[a-z]/.match?(c) 
-            new_ascii = c.ord + num
-            if new_ascii > 122
-                new_ascii = new_ascii - 26
-            end
-            cipher_text += new_ascii.chr
-        else
-            new_ascii = c.ord
-            cipher_text += new_ascii.chr
-        end 
+  cipher_text = ''
+  text.each_char do |c|
+    if /[A-Z]/.match?(c)
+      new_ascii = c.ord + num
+      if new_ascii > 90 then new_ascii -= 26 end
+    elsif /[a-z]/.match?(c)
+      new_ascii = c.ord + num
+      if new_ascii > 122 then new_ascii -= 26 end
+    else
+      new_ascii = c.ord
     end
-    return cipher_text
+    cipher_text += new_ascii.chr
+  end
+  cipher_text
 end
 
-puts ceaser_cipher("What a string!",5)
+puts ceaser_cipher('What a string!', 5)
